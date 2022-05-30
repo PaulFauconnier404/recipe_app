cbfcef886118296198cbc662aaabab85ac10dfeb
V3NG3URZ <84502806+V3NG3URZ@users.noreply.github.com>
2022-05-30 15:10:48 +0200

    Create recipe_provider and modif recipe_model
    Création du fichier recipe_provider et modification du fichier recipe_model pour récupérer les données de la base ( récupération recette)

---
+++ recipe_app/lib/model/recipe_provider.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:collection'; // nouveaux type de listes comme UnmodifiableListView
+import 'dart:convert'; // pour decoder la réponse http
+
+import 'package:recipe_app/model/recipe_model.dart';
+
+// Commandes utiles :
+// Lancer le serveur node (attendre le message "connexion ok !")
+// backend> npm start
+
+class RecipeProvider with ChangeNotifier {
+  final String host = 'http://localhost:3000';
+  List<Recipe> _recipe = [];
+
+  // Getter pour l'accès en lecture de l'ensemble des profiles
+  // Pas de modificiation possible grâce au type UnmodifiableListView
+  UnmodifiableListView<Recipe> get users => UnmodifiableListView(_recipe);
+
+  // Récupérer les données dans la base de données
+  void fetchData() async {
+    try {
+      http.Response response = await http.get(Uri.parse('$host/api/users'));
+      if (response.statusCode == 200) {
+        _recipe = (json.decode(response.body) as List)
+            .map((userJson) => Recipe.fromJson(userJson))
+            .toList();
+        notifyListeners();
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Ajouter un profile dans la base de données
+  Future<void> addUser(Recipe newUser) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse('$host/api/users'),
+        body: json.encode(newUser.toJson()),
+        headers: {'Content-type': 'application/json'},
+      );
+      if (response.statusCode == 200) {
+        _recipe.add(
+          Recipe.fromJson(
+            json.decode(response.body),
+          ),
+        );
+        notifyListeners();
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<String> logUser(Recipe user) async {
+    try {
+      http.Response response = await http.post(
+        Uri.parse('$host/api/users/login'),
+        body: json.encode(user.toJson()),
+        headers: {'Content-type': 'application/json'},
+      );
+      Map<String, dynamic> temp = json.decode(response.body);
+
+      if (response.statusCode == 200) {
+        return ("200");
+      } else {
+        return (temp['error']);
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
